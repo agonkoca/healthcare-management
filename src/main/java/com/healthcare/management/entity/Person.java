251c26f1a6517451b55c79e0157e43384a323d53
Amar Zhuri <amarzhurix@gmail.com>
2025-11-23 19:55:39 +0100

    Implemented Patient and Doctors entities

---
+++ src/main/java/com/healthcare/management/entity/Person.java
@@ -0,0 +1,52 @@
+package com.healthcare.management.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    protected String name;
+    protected String phone;
+    protected String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public abstract String getDetails();
+}
